3f466b3879ecc9ca45012b4444c19779d13ac5b7
N1OrBy <kulik.norbert09@gmail.com>
2024-06-04 20:56:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('change-text').addEventListener('click', function() {
+    document.getElementById('welcome').querySelector('p').textContent = 'A szöveg megváltozott! A semmi is lehet változatos és izgalmas, ha elég kreatívan közelítünk hozzá.';
+});
